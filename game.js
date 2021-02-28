af4a0ca9e495c1e4cc3a5eac53126454abcc1b1c
lrbn86 <lrbn86@gmail.com>
2021-02-28 15:21:18 -0500

    Added some comments for code clarity

--- game.js
+++ game.js
@@ -1,42 +1,54 @@
 import Player from './src/Player.js';
 import Projectile from './src/Projectile.js';
 
+/* SET UP CANVAS */
 const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const c = canvas.getContext('2d');
 
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
 
-const player = new Player(centerX, centerY, 30, 'blue', c);
+/* BEGIN GAME CODE */
 
-const projectiles = [];
-const projectileSize = 10;
-const projectileColor = 'green';
+const centerX = canvas.width / 2; // The center of canvas on x-axis
+const centerY = canvas.height / 2; // The center of canvas on y-axis
 
-// const projectile = new Projectile(centerX, centerY, 5, 'red', {x: 1, y: 1}, c);
+const player = new Player(centerX, centerY, 30, 'blue', c); // Player object
 
+const projectiles = []; // Array of projectiles
+const projectileSize = 10; // The projectile's radius
+const projectileColor = 'green'; // The color of the projectile
+
+// Render Loop
 function animate() {
 	requestAnimationFrame(animate);
-	c.clearRect(0, 0, canvas.width, canvas.height);
-	player.update();
+	c.clearRect(0, 0, canvas.width, canvas.height); // Clear the screen
+	player.update(); // Draw and update player
+
+	// Loop through projectile array and draw and update each projectile 
+	// Each projectile moves independently of each other
+	// and we can have multiple projectile at the same time on screen instead of just one projectile
 	projectiles.forEach((projectile) => {
 		projectile.update();
 	});
 }
 
 window.addEventListener('click', function(event) {
-	const mouseX = event.clientX;
-	const mouseY = event.clientY;
-	const angle = Math.atan2(mouseY - centerY, mouseX - centerX)
+	const mouseX = event.clientX; // The position of where the mouse clicked on the x-axis
+	const mouseY = event.clientY; // The position of where the mouse clicked on the y-axis
+	const angle = Math.atan2(mouseY - centerY, mouseX - centerX) // Calculate angle
+
+	// Calculate velocity
 	const velocity = {
 		x: Math.cos(angle),
 		y: Math.sin(angle)
 	};
+
+	// Create a new projectile that is initially in the center of the screen
+	// and follows where the mouse last clicked
 	const projectile = new Projectile(centerX, centerY, projectileSize, projectileColor, velocity, c);
-	projectiles.push(projectile);
+
+	projectiles.push(projectile); // Store projectile into projectiles array
 });
 
 animate();
