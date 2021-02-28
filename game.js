7d2029d5901b14d88cd4fcd2aae0a40ef9be5757
lrbn86 <lrbn86@gmail.com>
2021-02-28 15:09:56 -0500

    Added projectiles

--- game.js
+++ game.js
@@ -7,15 +7,36 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const x = canvas.width / 2;
-const y = canvas.height / 2;
-const player = new Player(x, y, 30, 'blue', c);
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
 
-player.draw();
+const player = new Player(centerX, centerY, 30, 'blue', c);
+
+const projectiles = [];
+const projectileSize = 10;
+const projectileColor = 'green';
+
+// const projectile = new Projectile(centerX, centerY, 5, 'red', {x: 1, y: 1}, c);
+
+function animate() {
+	requestAnimationFrame(animate);
+	c.clearRect(0, 0, canvas.width, canvas.height);
+	player.update();
+	projectiles.forEach((projectile) => {
+		projectile.update();
+	});
+}
 
 window.addEventListener('click', function(event) {
 	const mouseX = event.clientX;
 	const mouseY = event.clientY;
-	const projectile = new Projectile(mouseX, mouseY, 5, 'red', null, c);
-	projectile.draw();
+	const angle = Math.atan2(mouseY - centerY, mouseX - centerX)
+	const velocity = {
+		x: Math.cos(angle),
+		y: Math.sin(angle)
+	};
+	const projectile = new Projectile(centerX, centerY, projectileSize, projectileColor, velocity, c);
+	projectiles.push(projectile);
 });
+
+animate();
